b277ddcab67125aef60d0f40ec6807f2a77bd480
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-05-18 20:24:20 +0100

    fix removeComments
    `removeComments` now correctly removes single line and multiline
    comments from elm code.

--- src/RemoveComments.elm
+++ src/RemoveComments.elm
@@ -4,5 +4,6 @@ import Regex
 
 removeComments : String -> String
 removeComments =
-    Regex.replace Regex.All (Regex.regex "--.$") (always "")
+    Regex.replace Regex.All (Regex.regex "--.*") (always "")
+        >> Regex.replace Regex.All (Regex.regex "{-[\\s\\S]*-}") (always "")
         >> Regex.replace Regex.All (Regex.regex "\n +\\w+ : .*") (always "")
